0717e7163bec70d7a6bb70dd5f8dfc394d046f6d
joao.conceicao <joao.conceicao@bee-eng.pt>
2023-06-19 17:00:20 +0100

    cenas

--- SamurAI/urls.py
+++ SamurAI/urls.py
@@ -18,33 +18,23 @@ from django.urls import path, include
 from drf_yasg import openapi, views
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-from SamurAI.Controllers.DatasetsController import remove_from_dataset, retrieve_classifier, import_dataset, export_dataset, \
-    add_input_dataset, get_from_dataset
-from SamurAI.Controllers.ModelsController import classify_input, create_model, delete_model, train_model, update_model, get_models, \
-    order_models
+from SamurAI.Controllers.DatasetsController import DatasetsController
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="SamurAI API",
-        default_version='v1',
-        description="API for SamurAI",
-        terms_of_service="https://your-terms-of-service-url.com/",
-        contact=openapi.Contact(email="bee@bee-eng.pt"),
-        license=openapi.License(name="MIT License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
+from SamurAI.Controllers.ModelsController import (
+    classify_input, create_model, delete_model, train_model, update_model,
+    get_models, order_models
 )
+datasetscontroller = DatasetsController()
 
 urlpatterns = [
-    path('retrieve_classifier/', retrieve_classifier, name='retrieve_classifier'),
-    path('get_from_dataset/', get_from_dataset, name='get_from_dataset'),
-    path('import_dataset/', import_dataset, name='import_dataset'),
-    path('export_dataset/', export_dataset, name='export_dataset'),
-    path('add_input_dataset/', add_input_dataset, name='add_input_dataset'),
-    path('remove_from_dataset/', remove_from_dataset, name='remove_from_dataset'),
+    path('retrieve_classifier/', datasetscontroller.retrieve_classifier, name='retrieve_classifier'),
+    path('get_from_dataset/', datasetscontroller.get_from_dataset, name='get_from_dataset'),
+    path('import_dataset/', datasetscontroller.import_dataset, name='import_dataset'),
+    path('export_dataset/', datasetscontroller.export_dataset, name='export_dataset'),
+    path('add_input_dataset/', datasetscontroller.add_input_dataset, name='add_input_dataset'),
+    path('remove_from_dataset/', datasetscontroller.remove_from_dataset, name='remove_from_dataset'),
 
-    # URLs para as views do modelo
+    # URLs for the model views
     path('classify_input/', classify_input, name='classify_input'),
     path('create_model/', create_model, name='create_model'),
     path('delete_model/', delete_model, name='delete_model'),
